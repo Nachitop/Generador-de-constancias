ff2814c6aa323a9e4dc21356054da16560a79be1
Nachitop <nachitopsk@gmail.com>
2018-10-19 00:47:38 -0600

    primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace GeneradorConstancias
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.btnAbrirExcel = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_abrirWord = new System.Windows.Forms.Button();
+            this.txtRutaWord = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.lblProgreso = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Enabled = false;
+            this.btnGenerar.Location = new System.Drawing.Point(658, 21);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(200, 57);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Archivo excel";
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Enabled = false;
+            this.txtRuta.Location = new System.Drawing.Point(319, 21);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(230, 20);
+            this.txtRuta.TabIndex = 2;
+            // 
+            // btnAbrirExcel
+            // 
+            this.btnAbrirExcel.Location = new System.Drawing.Point(567, 21);
+            this.btnAbrirExcel.Name = "btnAbrirExcel";
+            this.btnAbrirExcel.Size = new System.Drawing.Size(62, 23);
+            this.btnAbrirExcel.TabIndex = 3;
+            this.btnAbrirExcel.Text = "...";
+            this.btnAbrirExcel.UseVisualStyleBackColor = true;
+            this.btnAbrirExcel.Click += new System.EventHandler(this.btnAbrirExcel_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_abrirWord
+            // 
+            this.btn_abrirWord.Location = new System.Drawing.Point(567, 70);
+            this.btn_abrirWord.Name = "btn_abrirWord";
+            this.btn_abrirWord.Size = new System.Drawing.Size(62, 23);
+            this.btn_abrirWord.TabIndex = 6;
+            this.btn_abrirWord.Text = "...";
+            this.btn_abrirWord.UseVisualStyleBackColor = true;
+            this.btn_abrirWord.Click += new System.EventHandler(this.btn_abrirWord_Click);
+            // 
+            // txtRutaWord
+            // 
+            this.txtRutaWord.Enabled = false;
+            this.txtRutaWord.Location = new System.Drawing.Point(319, 70);
+            this.txtRutaWord.Name = "txtRutaWord";
+            this.txtRutaWord.Size = new System.Drawing.Size(230, 20);
+            this.txtRutaWord.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(125, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Archivo word";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(658, 109);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(200, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // lblProgreso
+            // 
+            this.lblProgreso.AutoSize = true;
+            this.lblProgreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProgreso.Location = new System.Drawing.Point(655, 91);
+            this.lblProgreso.Name = "lblProgreso";
+            this.lblProgreso.Size = new System.Drawing.Size(57, 15);
+            this.lblProgreso.TabIndex = 9;
+            this.lblProgreso.Text = "Progreso";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(435, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Desarrollado por: Sánchez Mundo Martín Ignacio y Acosta Rocha Jesús Carlos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(875, 168);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblProgreso);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_abrirWord);
+            this.Controls.Add(this.txtRutaWord);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnAbrirExcel);
+            this.Controls.Add(this.txtRuta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "Form1";
+            this.Text = "Generador de constancias";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.Button btnAbrirExcel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_abrirWord;
+        private System.Windows.Forms.TextBox txtRutaWord;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label lblProgreso;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using Microsoft.Office.Interop.Word;
+
+
+
+
+
+namespace GeneradorConstancias
+{
+    
+    
+
+    public partial class Form1 : Form
+    {
+        Microsoft.Office.Interop.Excel.Application _excelApp;
+        Workbook book;
+        Workbook bookWord;
+        string nombreArchivo;
+        string nombreArchivoWord;
+        Microsoft.Office.Interop.Word.Application _wordApp;
+
+        int progreso;
+        InfoComun infocomun = new InfoComun();
+        List<Alumno> alumnos=  new List<Alumno>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAbrirExcel_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result==DialogResult.OK)
+            {
+                this.nombreArchivo = openFileDialog1.FileName;
+                txtRuta.Text = this.nombreArchivo;
+                habilitarBoton();
+            }
+         
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            _excelApp = new Microsoft.Office.Interop.Excel.Application();
+            book = _excelApp.Workbooks.Add(this.nombreArchivo);
+            Worksheet sheet = (Worksheet)book.Sheets[1];
+            Worksheet sheet2 = (Worksheet)book.Sheets[2];
+
+            
+            infocomun.Carrera = sheet2.Cells[2, 1].value;
+            infocomun.Grado_asesor = sheet2.Cells[2, 2].value;
+            //MessageBox.Show(infocomun.Grado_asesor);
+            infocomun.Nombre_asesor= sheet2.Cells[2, 3].value;
+            infocomun.Ciclo = sheet2.Cells[2, 4].value;
+            infocomun.Fecha = sheet2.Cells[2, 5].value;
+            sheet.Rows.ClearFormats();
+            for (int x=2;x<=sheet.UsedRange.Rows.Count;x++)
+            {
+                
+                
+                   // MessageBox.Show(sheet.Cells[x, 1].value);
+                    Alumno alumno = new Alumno();
+                    alumno.Nombre = sheet.Cells[x, 1].value;
+                    alumno.Proyecto = sheet.Cells[x, 2].value;
+                    //alumno.Lugar = sheet.Cells[x, 3].value;
+                    //alumno.Fecha = sheet.Cells[x, 4].value;
+                  //  MessageBox.Show(alumno.Proyecto);
+                    alumnos.Add(alumno);
+                
+
+            }
+            actualizarProgreso(30, "leyendo excel..");
+
+            book.Close();
+
+            generarConstancias();  
+         
+        }
+
+           public void actualizarProgreso(int valor, string comentario)
+        {
+            progreso += valor;
+            System.Threading.Thread.Sleep(1000);
+            progressBar1.Value = progreso;
+            lblProgreso.Text = comentario;
+            if (progressBar1.Value == 100)
+            {
+                MessageBox.Show("CONSTANCIAS GENERADAS");
+                progressBar1.Value = 0;
+                lblProgreso.Text = "Progreso";
+                progreso = 0;
+                alumnos.Clear();
+            }
+        }
+        private void habilitarBoton()
+        {
+            if(txtRuta.Text!="" && txtRutaWord.Text != "")
+            {
+                btnGenerar.Enabled = true;
+            }
+        }
+        private void generarConstancias()
+        {
+            foreach( var alumno in alumnos)
+            {
+                _wordApp = new Microsoft.Office.Interop.Word.Application();
+                Document wordDoc = _wordApp.Documents.Add(this.nombreArchivoWord);
+                Bookmark nombre_alumno = wordDoc.Bookmarks["nombre_alumno"];
+                Bookmark nombre_proyecto = wordDoc.Bookmarks["nombre_proyecto"];
+                Bookmark nombre_asesor = wordDoc.Bookmarks["nombre_asesor"];
+                Bookmark grado_asesor = wordDoc.Bookmarks["grado_asesor"];
+                Bookmark fecha = wordDoc.Bookmarks["fecha"];
+                Bookmark ciclo = wordDoc.Bookmarks["ciclo"];
+                Bookmark carrera = wordDoc.Bookmarks["carrera"];
+                Microsoft.Office.Interop.Word.Range nombre_alumno_r = nombre_alumno.Range;
+                Microsoft.Office.Interop.Word.Range nombre_proyecto_r = nombre_proyecto.Range;
+                Microsoft.Office.Interop.Word.Range nombre_asesor_r = nombre_asesor.Range;
+                Microsoft.Office.Interop.Word.Range grado_asesor_r = grado_asesor.Range;
+                Microsoft.Office.Interop.Word.Range fecha_r = fecha.Range;
+                Microsoft.Office.Interop.Word.Range ciclo_r = ciclo.Range;
+                Microsoft.Office.Interop.Word.Range carrera_r = carrera.Range;
+
+                nombre_alumno_r.Text = alumno.Nombre;
+                nombre_proyecto_r.Text = alumno.Proyecto;
+                grado_asesor_r.Text = infocomun.Grado_asesor;
+                nombre_asesor_r.Text = infocomun.Nombre_asesor;
+                fecha_r.Text = infocomun.Fecha;
+                ciclo_r.Text = infocomun.Ciclo;
+                carrera_r.Text = infocomun.Carrera;
+                string nomArchivoNuevo;
+                nomArchivoNuevo = alumno.Nombre.ToString().Trim() + "_" + infocomun.Ciclo.ToString().Trim() + ".docx";
+               // MessageBox.Show(nomArchivoNuevo);
+               
+                wordDoc.SaveAs2(@"C:\Constancias\" + nomArchivoNuevo);
+                wordDoc.Close();
+
+
+            }
+            actualizarProgreso(70, "generando constancias...");
+        }
+
+        private void btn_abrirWord_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                this.nombreArchivoWord = openFileDialog2.FileName;
+                txtRutaWord.Text = this.nombreArchivoWord;
+                habilitarBoton();
+            }
+          
+        }
+    }
+}
+public class Alumno
+{
+
+    string nombre;
+    string proyecto;
+   // string lugar;
+    //string fecha;
+
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Proyecto { get => proyecto; set => proyecto = value; }
+   // public string Lugar { get => lugar; set => lugar = value; }
+    //public string Fecha { get => fecha; set => fecha = value; }
+}
+
+public class InfoComun
+{
+
+    string carrera;
+    string grado_asesor;
+    string nombre_asesor;
+    string fecha;
+    string ciclo;
+
+    public string Carrera { get => carrera; set => carrera = value; }
+    public string Grado_asesor { get => grado_asesor; set => grado_asesor = value; }
+    public string Nombre_asesor { get => nombre_asesor; set => nombre_asesor = value; }
+    public string Fecha { get => fecha; set => fecha = value; }
+    public string Ciclo { get => ciclo; set => ciclo = value; }
+}
